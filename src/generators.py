fb18172cba85cb047e8ae721e61eb87fac96ac48
artem <artem.nazarenko.2002@bk.ru>
2026-01-09 20:28:08 +0300

    новый модуль с генеарторами

---
+++ src/generators.py
@@ -0,0 +1,129 @@
+from typing import Iterator, Generator
+
+
+
+transactions = (
+    [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+)
+def filter_by_currency(iterations) -> Iterator:
+    for iteration in iterations:
+        if iteration.get("operationAmount",{}).get("currency",{}).get("code") == "USD":
+            yield iteration
+
+
+
+def transaction_descriptions(somethings) -> Iterator:
+    for some in somethings:
+        yield some.get("description","описание не найдено")
+
+
+
+
+def card_number_generator(start, stop) -> Generator:
+    for x in range(start,stop+1):
+        letter = f"{x:0>16}"
+        last_result = " ".join([letter[i:i+4] for i in range(0,16,4)])
+        # result1 = letter[0:4]
+        # result2 = letter[4:8]
+        # result3 = letter[8:12]
+        # result4 = letter[12:16]
+        # last_result = result1 + " " + result2 + " " + result3 + " " + result4
+        yield last_result
+
+result = card_number_generator(1,11)
+
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+
+
+
+
+
+
+
+
